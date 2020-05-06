dd4575e73e088ed368ff6e0a4d810052f7e49349
alshchetinin <alexsander.qw@gmail.com>
2020-05-06 18:31:12 +0300

    first commit

--- single.php
+++ single.php
@@ -16,11 +16,6 @@
 <div class="container">
    <div class="row">
       <div class="col-md-12" style="margin-bottom: 40px">
-         <?php the_post_thumbnail('full', 'class=img-responsive') ?>
-      </div>
-   </div>
-   <div class="row">
-      <div class="col-md-12" style="margin-bottom: 40px">
          <?php the_post(); the_content();?>
       </div>
    </div>
