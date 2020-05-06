3f4bcf09f9c91f50ac58caa6849a03929cd8c29f
alshchetinin <alexsander.qw@gmail.com>
2020-05-06 18:34:31 +0300

    fix icon size

--- blocks-editor/tickList.php
+++ blocks-editor/tickList.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $title = get_field( 'title' );
@@ -7,15 +6,15 @@ $text = get_field('text')
 ?>
 
 <div class="tick-list">
-   <div class="tick-list__icon"><img src="<?php echo $icon?>" alt="" width="28px"></div>
+   <div class="tick-list__icon"><img src="<?php echo $icon?>" alt="" width="60px"></div>
    <div class="tick-list__content">
       <div class="tick-list__title">
          <?php echo $title?>
       </div>
       <div class="tick-list__text">
-      <?php echo $text?>
+         <?php echo $text?>
       </div>
-      
+
    </div>
-   
+
 </div>
